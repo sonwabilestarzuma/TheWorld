1c0f5a3809fd2a35d65b9cf6c35ebc0327a52e24
Star Sonwabo Zuma <sonwabilestarzuma@outlook.com>
2019-06-04 01:10:03 -0600

    initial commit

--- TheWorld/wwwroot/js/app-trips.js
+++ TheWorld/wwwroot/js/app-trips.js
@@ -1,20 +1,9 @@
-﻿// app-trips-js
+﻿// app-trips.js
 
 (function () {
 
     "use strict";
-
     // Creating the module
-    angular.module("app-trips", ["simpleControls", "ngRoute"])
-        .config(function ($routProvider) {
-
-            $routeProvider.when("/", {
-                controller: "tripsController",
-                controllerAs: "vm",
-                templateUrl: "/views/tripsView.html"
-            });
-            $routeProvider.otherwise({ redirectTo: "/" });
-        });
+    angular.module("app-trips", []);
 
 })();
-
--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -1,77 +1,26 @@
-﻿// tripsController.js
+﻿// tripsController.js 
 
 (function () {
 
     "use strict";
 
-	// Getting the existing module
-
+    // Getting the existing module
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController($http) {
+    function tripsController() {
 
         var vm = this;
-        vm.trips = [];
+        vm.trips = [{
 
+            name: "US Trip",
+            created: new Date()
+        }, {
 
-            //[{
+                name: "World Trip",
+                created: new Date()
 
-   //         name: "US Trip",
-			//created: new Date()
-   //     },
-   //         {
-   //         name: "SA Trip",
-   //         created: new Date()
-   //     },
-   //         {
-			//	name: "World Trip",
-			//	created: new Date()
-   //         }];
-
-        // object for getting new trip using input 
-        vm.newTrip = {};
-        vm.errorMessage = "";
-
-        // busy flag
-        vm.isBusy = true;
-
-        $http.get("/api/trips")
-            .then(function (response) {
-                //Success
-                angular.copy(response.data, vm.trips);
-            }, function () {
-                //Failure
-                vm.errorMessage = "Failed to load data: " + Error;
-            })
-            .finally(function () {
-              vm.isBusy = false;
-            });
-        // pushing data to the database 
-        vm.addTrip = function () {
-            //vm.trips.push({
-            //    name: vm.newTrip.name, created: new Date()
-            
-            //});
-            //// clear the form 
-            //vm.newTrip = {};
-
-            vm.isBusy = true;
-            vm.errorMessage = "";
-            $http.post("/api/trips", vm.newTrip)
-                .then(function (response) {
-                    //success
-                    vm.trips.push(response.data);
-                    vm.newTrip = {};
-                }, function () {
-                    // failure
-                    vm.errorMessage = " Failed to save new trip";
-                })
-                .finally(function () {
-                    vm.isBusy = false;
-                });
-        };
-
-
+        }];
     }
+
 })();
