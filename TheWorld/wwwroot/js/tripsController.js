fd99041bb1bbe4fce950d0b65b137cb270da1a79
Sonwabo Zuma <sonwabilestarzuma@outlook.com>
2019-06-05 12:09:30 +0200

    added angular in my project

--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -8,19 +8,57 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
-        vm.trips = [{
+        vm.trips = [];
 
-            name: "US Trip",
-            created: new Date()
-        }, {
+        vm.newTrip = {};
 
-                name: "World Trip",
-                created: new Date()
+        vm.errorMessage = "";
+        vm.isBusy = true;
 
-        }];
+        $http.get("/api/trips")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+               vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            $http.post("/api/trips", vm.newTrip)
+                .then(function (repsonse) {
+                    // Success
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+
+                }, function () {
+                    //failure
+                    vm.errorMessage = "Failed to save new trip";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+
+                });
+
+            //vm.trips.push({
+            //    name: vm.newTrip.name,
+            //    created: new Date()
+            //});
+
+            //vm.newTrip = {};
+        };
+
+
     }
 
 })();
